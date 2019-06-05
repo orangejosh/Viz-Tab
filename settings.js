a3f0d42bfdd0e835562663085d68b4be365852a6
Josh <joshua.lytle@gmail.com>
2019-06-05 18:49:20 -0500

    Added a selector in the settings to add a background image. It doesn't completely work yet

--- settings.js
+++ settings.js
@@ -12,9 +12,11 @@ function init(){
 
 		var newTabButton = document.getElementById("newTab");
 		var overrideButton = document.getElementById("overrideTab");
+		var pickButton = document.getElementById("file-input");
 
 		newTabButton.addEventListener('change', saveNewTabSetting, false);
 		overrideButton.addEventListener('change', saveOverrideSetting, false);
+		pickButton.addEventListener('input', loadImage, false);
 
 		if (data.openTab){
 			newTabButton.checked = true;
@@ -39,5 +41,18 @@ function saveOverrideSetting() {
 		data.newTab= !data.newTab;
 		chrome.storage.local.set(data);
 	})
+
+}
+
+function loadImage(e){
+	var file = e.target.files[0];
+	var reader = new FileReader();
+	reader.readAsDataURL(file); 
+
+	reader.onload = readerEvent => {
+		var content = readerEvent.target.result;
+		var options = ' no-repeat center center fixed';
+		document.body.style.backgroundImage = 'url('+ content +')';
+	}
 
 }
