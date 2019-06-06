c70464a12886b76eebd163051a713a65488fe052
Josh <joshua.lytle@gmail.com>
2019-06-06 18:50:12 -0500

    Can now add a background image and change the color of the block in the settings. It also saves those two things now. Doesn't work yet on regular screen

--- settings.js
+++ settings.js
@@ -1,23 +1,36 @@
 
+var color;
 document.onload = init();
 
 function init(){
 	chrome.storage.local.get(null, function(data){
+		var newTabButton = document.getElementById("newTab");
+		var overrideButton = document.getElementById("overrideTab");
+		var pickButton = document.getElementById("file-input");
+		var clearButton = document.getElementById("clear");
+		var colorButton = document.getElementById("color-picker");
+
+		newTabButton.addEventListener('change', saveNewTabSetting, false);
+		overrideButton.addEventListener('change', saveOverrideSetting, false);
+		pickButton.addEventListener('input', loadImage, false);
+		clearButton.addEventListener('click', clearBackground, false);
+		colorButton.addEventListener('input', getColor, false);
+		colorButton.addEventListener('focus', setColor, false);
+
 		if (data.openTab === undefined){
 			data.openTab = false;
 		}
 		if (data.newTab === undefined) {
 			data.newTab = false;
 		}
-
-		var newTabButton = document.getElementById("newTab");
-		var overrideButton = document.getElementById("overrideTab");
-		var pickButton = document.getElementById("file-input");
-
-		newTabButton.addEventListener('change', saveNewTabSetting, false);
-		overrideButton.addEventListener('change', saveOverrideSetting, false);
-		pickButton.addEventListener('input', loadImage, false);
-
+		if (data.backgroundImg !== undefined) {
+			document.body.style.backgroundImage = 'url(' + data.backgroundImg + ')';
+		}
+		if (data.color !== undefined) {
+			document.getElementById('container').style.backgroundColor = data.color;
+			color = data.color;
+			colorButton.value = data.color;
+		}
 		if (data.openTab){
 			newTabButton.checked = true;
 		}
@@ -51,8 +64,33 @@ function loadImage(e){
 
 	reader.onload = readerEvent => {
 		var content = readerEvent.target.result;
-		var options = ' no-repeat center center fixed';
 		document.body.style.backgroundImage = 'url('+ content +')';
-	}
 
+		chrome.storage.local.get(null, (data) => {
+			data.backgroundImg = content;
+			chrome.storage.local.set(data);
+		});
+	}
+}
+
+function clearBackground() {
+	chrome.storage.local.get(null, (data) => {
+		data.backgroundImg = null;
+		chrome.storage.local.set(data);
+		document.body.style.backgroundImage = null;
+	});
+}
+
+
+
+function getColor(e) {
+	color = e.target.value;
+}
+
+function setColor() {
+	document.getElementById('container').style.backgroundColor = color;
+	chrome.storage.local.get(null, (data) => {
+		data.color = color;
+		chrome.storage.local.set(data);
+	});
 }
